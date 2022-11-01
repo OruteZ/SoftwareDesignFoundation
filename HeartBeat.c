0b26147d8a5f2cb41ab16a35394cc021e5c85291
OruteZ <47718603+OruteZ@users.noreply.github.com>
2022-11-01 15:28:43 +0900

    2

--- HeartBeat.c
+++ HeartBeat.c
@@ -4,12 +4,13 @@ void HeartBeat_init()
 {
 	heart_beat = (HeartBeat*)malloc(sizeof(HeartBeat));
 
-	heart_beat->note_size = 15;
-	heart_beat->BPM = 420;
+	heart_beat->note_size = 40;
+	heart_beat->BPM = 240;
 	heart_beat->time_to_check_tempo = 0;
+	heart_beat->combo = 0;
 
 	heart_beat->note = (short*)malloc(sizeof(short) * heart_beat->note_size);
-	heart_beat->print_buffer = (char*)malloc(sizeof(char) * 2 * heart_beat->note_size);
+	heart_beat->print_buffer = (char*)malloc(sizeof(char) * (heart_beat->note_size * 2));
 	for (int i = 0; i < heart_beat->note_size; i++) {
 		heart_beat->note[i] = 0;
 		heart_beat->print_buffer[i * 2] = ' ';
@@ -17,7 +18,11 @@ void HeartBeat_init()
 	}
 
 
-	heart_beat->print_point = Point_new(0, 10);
+	heart_beat->print_point = Point_new(0, 20);
+}
+
+unsigned _stdcall Thread_PlaySound(void* arg) {
+	Beep(3000, 100);
 }
 
 void HeartBeat_update(double delta_time)
@@ -29,25 +34,39 @@ void HeartBeat_update(double delta_time)
 		heart_beat->time_to_check_tempo -= ((double)60 * 1000) / (double)heart_beat->BPM;
 	}
 
-	if (_kbhit()) {
-		_getch();
-
-		isHit = HeartBeat_is_note_beaten();
+	if (GetKeyDown('J')) {
+		if (HeartBeat_is_note_beaten()) {
+			isHit = TRUE;
+			if (++(heart_beat->combo) >= 10) {
+				HeartBeat_reset();
+				heart_beat->combo = 0;
+				heart_beat->BPM += 120;
+			}
+		}
+		else
+		{
+			heart_beat->combo = 0;
+		}
 	}
 }
 
 void HeartBeat_render()
 {
-	for (int dy = 0; dy < 4; dy++) 
+	for (int dy = 0; dy < 4; dy++) {
 		ScreenPrint(heart_beat->print_point->x, heart_beat->print_point->y + dy, heart_beat->print_buffer);
-
-	//test
-	if (isHit) ScreenPrint(0, 2, "AAAA");
+		ScreenPrint(
+			heart_beat->print_point->x + heart_beat->note_size * 2,
+			heart_beat->print_point->y + dy,
+			"                    "
+		);
+	}
 }
 
 void HeartBeat_release()
 {
 	free(heart_beat->note);
+	free(heart_beat->print_buffer);
+	Point_delete(heart_beat->print_point);
 	free(heart_beat);
 }
 
@@ -63,23 +82,31 @@ void HeartBeat_move_note()
 		heart_beat->note[i - 1] = heart_beat->note[i];
 		if (heart_beat->note[i - 1] == 1) last_node_point = i;
 	}
-	
-	if(last_node_point <= size - 4) 
+
+	if (last_node_point <= size - 4) {
 		heart_beat->note[size - 1] = 1;
-	else 
-		heart_beat->note[size - 1] = 0;
+		_beginthreadex(NULL, 0, Thread_PlaySound, 0, 0, NULL);
+	}
+
+	else heart_beat->note[size - 1] = 0;
 	HeartBeat_reload_buffer();
 }
 
 void HeartBeat_reload_buffer()
 {
 	int i, size = heart_beat->note_size;
-
 	for (i = 0; i < size; i++) {
 		//sprintf((*fps_data)->fps_text_buffer, "FPS=%d", (*fps_data)->frame_cnt);
 		if (heart_beat->note[i] == 1)
-			sprintf(heart_beat->print_buffer + (i * 2), "бс");
-		else sprintf(heart_beat->print_buffer + (i * 2), "  ");
+			sprintf((heart_beat->print_buffer) + (i * 2), "бс");
+		else sprintf((heart_beat->print_buffer) + (i * 2), "  ");
+	}
+}
+
+void HeartBeat_reset()
+{
+	for (int i = 0; i < heart_beat->note_size; i++) {
+		heart_beat->note[i] = 0;
 	}
 }
 
@@ -96,7 +123,7 @@ int HeartBeat_is_note_beaten()
 
 void HeartBeat_set_BPM(int BPM)
 {
-	heart_beat -> BPM = BPM;
+	heart_beat->BPM = BPM;
 }
 
 int HeartBeat_get_BPM()
@@ -109,4 +136,8 @@ short* HeartBeat_get_note_information()
 	short* result = (short*)malloc(sizeof(short) * heart_beat->note_size);
 	memcpy(result, heart_beat->note, heart_beat->note_size);
 	return result;
+}
+
+int HeartBeat_get_combo() {
+	return heart_beat->combo;
 }
