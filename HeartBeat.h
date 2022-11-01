75ec6e231b212bfd6befe74f7a022de23cd46565
OruteZ <47718603+OruteZ@users.noreply.github.com>
2022-11-01 15:27:50 +0900

    1

--- HeartBeat.h
+++ HeartBeat.h
@@ -4,9 +4,11 @@
 #include<string.h>
 #include"Point.h"
 #include"Screen.h"
+#include"KeyBoard.h"
+
 
 //for test
-#include<conio.h>
+#include <process.h>
 
 BOOLEAN isHit;
 //----
@@ -16,6 +18,7 @@ typedef struct _HeartBeatObject {
 	int BPM;
 	int note_size;
 	double time_to_check_tempo;
+	int combo;
 
 	Point* print_point;
 	char* print_buffer;
@@ -28,6 +31,8 @@ void HeartBeat_update(double delta_time);
 void HeartBeat_render();
 void HeartBeat_release();
 
+void HeartBeat_reset();
+
 void HeartBeat_move_note();
 void HeartBeat_reload_buffer();
 
@@ -38,4 +43,8 @@ void HeartBeat_set_BPM(int BPM);
 int HeartBeat_get_BPM();
 
 short* HeartBeat_get_note_information();
+
+int HeartBeat_get_combo();
+
+unsigned _stdcall Thread_PlaySound(void* arg);
 
