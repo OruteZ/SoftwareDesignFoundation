6b8a03517e8a79da589c330f979a879225403078
OruteZ <47718603+OruteZ@users.noreply.github.com>
2022-11-17 16:35:26 +0900

    .

--- Camera.c
+++ Camera.c
@@ -6,17 +6,21 @@
 #include "World.h"
 #include "QuadTree.h"
 
-int _cameraWidth = 11;
-int _cameraHeight = 9;
+int _cameraWidthInGame = 11;
+int _cameraHeightInGame = 9;
 
 Rect CameraRectInGame;
-
 Rect CameraRectInCanvas;
 
 void InitCamera()
 {
-	CameraRectInGame.height = _cameraHeight;
-	CameraRectInGame.width = _cameraWidth;
+	CameraRectInGame.height = _cameraHeightInGame;
+	CameraRectInGame.width = _cameraWidthInGame;
+}
+
+Point IngamePosition_to_CanvasPosition(Point pos)
+{
+
 }
 
 void PrintWorld()
@@ -41,8 +45,8 @@ void SetCameraPoint()
 {
 	Point playerPos = GetPlayerPos();
 
-	CameraRectInGame.x = playerPos.x - (_cameraWidth / 2);
-	CameraRectInGame.y = playerPos.y - (_cameraHeight / 2);
+	CameraRectInGame.x = playerPos.x - (_cameraWidthInGame / 2);
+	CameraRectInGame.y = playerPos.y - (_cameraHeightInGame / 2);
 
 	if (CameraRectInGame.x < 0) CameraRectInGame.x = 0;
 	if (CameraRectInGame.y < 0) CameraRectInGame.y = 0;
@@ -51,6 +55,6 @@ void SetCameraPoint()
 	int worldWidth = worldInfo->width;
 	int worldHeight = worldInfo->height;
 
-	if (CameraRectInGame.x + CameraRectInGame.width >= worldWidth) CameraRectInGame.x = worldWidth - _cameraWidth;
-	if (CameraRectInGame.y + CameraRectInGame.height >= worldHeight) CameraRectInGame.y = worldHeight - _cameraHeight;
+	if (CameraRectInGame.x + CameraRectInGame.width >= worldWidth) CameraRectInGame.x = worldWidth - _cameraWidthInGame;
+	if (CameraRectInGame.y + CameraRectInGame.height >= worldHeight) CameraRectInGame.y = worldHeight - _cameraHeightInGame;
 }
