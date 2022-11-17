530490c4a173e576ea7993b26a7eb84810a2013a
OruteZ <47718603+OruteZ@users.noreply.github.com>
2022-11-17 16:16:38 +0900

    .

--- Camera.c
+++ Camera.c
@@ -3,20 +3,28 @@
 #include<stdio.h>
 #include "Camera.h"
 #include "Player.h"
-
-Point CameraPoint;
+#include "World.h"
+#include "QuadTree.h"
 
 int _cameraWidth = 11;
 int _cameraHeight = 9;
 
 Rect CameraRect;
 
+void InitCamera()
+{
+	CameraRect.height = _cameraHeight;
+	CameraRect.width = _cameraWidth;
+}
+
 void PrintWorld()
 {
+	//2중 for문으로 돌면서 싹 출력
 }
 
 void PrintMortal()
 {
+
 }
 
 void PrintExpOrb()
@@ -27,8 +35,16 @@ void SetCameraPoint()
 {
 	Point playerPos = GetPlayerPos();
 
-	CameraPoint.x = playerPos.x - (_cameraWidth / 2);
-	CameraPoint.y = playerPos.y - (_cameraHeight / 2);
+	CameraRect.x = playerPos.x - (_cameraWidth / 2);
+	CameraRect.y = playerPos.y - (_cameraHeight / 2);
 
-	if(CameraPoint.x < 0)
+	if (CameraRect.x < 0) CameraRect.x = 0;
+	if (CameraRect.y < 0) CameraRect.y = 0;
+
+	World* worldInfo = GetCurrentWorld();
+	int worldWidth = worldInfo->width;
+	int worldHeight = worldInfo->height;
+
+	if (CameraRect.x + CameraRect.width >= worldWidth) CameraRect.x = worldWidth - _cameraWidth;
+	if (CameraRect.y + CameraRect.height >= worldHeight) CameraRect.y = worldHeight - _cameraHeight;
 }
