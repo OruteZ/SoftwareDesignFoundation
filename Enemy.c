f8109248fef081a6476e78a63ecdf977c65d0f2d
OruteZ <47718603+OruteZ@users.noreply.github.com>
2022-11-28 04:18:59 +0900

    Melee Enemy Attack Particle bug fix

--- Enemy.c
+++ Enemy.c
@@ -4,6 +4,7 @@
 #include "Game.h"
 #include "World.h"
 #include "Point.h"
+#include "Time.h"
 
 #include "Entity.h"
 #include "Enemy.h"
@@ -14,6 +15,17 @@
 #include "MeleeEnemy.h"
 #include "ArcherEnemy.h"
 #include "BomberEnemy.h"
+
+bool isEnemyDead(Enemy* enemy);
+bool canEnemyMove(Enemy* enemy);
+bool isEnemy(Entity* entity);
+bool canEnemyAttack(Enemy* enemy);
+
+void LookAt(Enemy* enemy, Point target);
+bool IsPlayerInRange(Enemy* enemy);
+void EnemyMove(Enemy* enemy, Point direction);
+void EnemyAttack(Enemy* enemy);
+void CalEnemyCooldown(Enemy* enemy);
 
 void LookAt(Enemy* enemy, Point target) {
 	int deltaX = target.x - enemy->base.entity.pos.x;
@@ -79,12 +91,9 @@ bool canEnemyAttack(Enemy* enemy) {
 }
 
 void EnemyAttack(Enemy* enemy) {
-	if (!canEnemyAttack(enemy)) return;
-
-#ifdef DEBUG
-	DebugPrint("Enemy can attack");
-	DebugPrint("Enemy type : %d", enemy->base.entity.type);
-#endif
+	if (!canEnemyAttack(enemy)) {
+		return;
+	}
 
 	switch (enemy->base.entity.type) {
 	case MeleeEnemyType:
@@ -99,6 +108,11 @@ void EnemyAttack(Enemy* enemy) {
 		BomberEnemyAttack((BomberEnemy*)enemy);
 		break;
 	}
+}
+
+void CalEnemyCooldown(Enemy* enemy) {
+	enemy->attackDelay -= Time.deltaTime;
+	enemy->moveCoolDown -= Time.deltaTime;
 }
 
 void EnemyOnDeath(Enemy* enemy)
@@ -121,7 +135,7 @@ void EnemyOnHit(Enemy* enemy, int damage)
 }
 
 void CreateEnemy(enum EntityType type, Point spawnPoint) {
-	Enemy* newEnemy;
+	Enemy* newEnemy = NULL;
 
 	switch (type) {
 	case MeleeEnemyType:
@@ -139,7 +153,9 @@ void CreateEnemy(enum EntityType type, Point spawnPoint) {
 	default: break;
 	}
 
-	if (newEnemy == NULL) exit(-1);
+#ifdef DEBUG
+	DebugPrint("%d %d", newEnemy->base.entity.type, type);
+#endif
 	VectorInsert(enemies, newEnemy);
 	//QuadTreeInsert(enemiesTree, newEnemy);
 }
@@ -150,6 +166,7 @@ void UpdateEnemy(Enemy* enemy) {
 		LookAt(enemy, GetPlayerPos());
 		EnemyAttack(enemy);
 	}
+	CalEnemyCooldown(enemy);
 }
 
 bool isEnemyDead(Enemy* enemy) {
@@ -164,3 +181,4 @@ bool isEnemy(Entity* entity) {
 bool canEnemyMove(Enemy* enemy) {
 	return enemy->moveCoolDown <= 0;
 }
+
