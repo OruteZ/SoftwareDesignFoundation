515b401b19f3938a776744f38faa59bbd6025b2f
OruteZ <47718603+OruteZ@users.noreply.github.com>
2022-11-29 09:12:12 +0900

    Change Init

--- Init.c
+++ Init.c
@@ -39,7 +39,7 @@ void Init()
     // StartWorld001();
 
     // Vectors
-    player = CreatePlayer();
+    player = CreatePlayer(GetCurrentWorld()->playerSpawnPoint);
     enemies = CreateVector();
     expOrbs = CreateVector();
     particles = CreateVector();
