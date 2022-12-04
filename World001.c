1f0d65d5f7d16fc40e4b270ea64db65da725dbc7
zzani0207 <87990208+zzani0207@users.noreply.github.com>
2022-12-04 14:53:08 +0900

    X coordinate of world001

--- World001.c
+++ World001.c
@@ -4,7 +4,8 @@
 #define W WALL,
 #define P PIT,
 #define D DOWNSTAIRS,
-	//
+  //1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 
+  //0                10                  20                  30                  40                  50                  60                  70                  80                  90
 Tile grid_001[] = {
 	P P P P P P P W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W W P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P 
 	P P P P P P P W G G G G G G G G G G G G W G G G G G G G G G G G G G G G G G G G G G G G G G G G G W P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P P 
